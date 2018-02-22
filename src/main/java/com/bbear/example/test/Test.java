1b42736b8e370b170922e5108572612a5ea56e79
BBear <chenjx0515@163.com>
2018-02-22 09:46:11 +0800

    python demo

---
+++ src/main/java/com/bbear/example/test/Test.java
@@ -0,0 +1,12 @@
+package com.bbear.example.test;
+
+/**
+ * @author junxiongchen
+ * @date 2018/02/08
+ */
+public class Test {
+    public static void main(String[] args) {
+        int i = 1 << 30;
+        System.out.println(i);
+    }
+}
