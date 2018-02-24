1d27357779f0b653f0fc89d7af60e6a8e9f37bad
BBear <chenjx0515@163.com>
2018-02-24 11:30:35 +0800

    test

---
+++ src/main/java/com/bbear/example/lintCode/Test2.java
@@ -0,0 +1,8 @@
+package com.bbear.example.lintCode;
+
+/**
+ * @author junxiongchen
+ * @date 2018/02/23
+ */
+public class Test2 {
+}
---
+++ src/main/java/com/bbear/example/lintCode/Test3.java
@@ -0,0 +1,8 @@
+package com.bbear.example.lintCode;
+
+/**
+ * @author junxiongchen
+ * @date 2018/02/24
+ */
+public class Test3 {
+}
