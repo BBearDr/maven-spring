5544ad665c8940da17175632b754e25842756310
BBear <chenjx0515@163.com>
2018-02-24 11:31:50 +0800

    test

--- src/main/java/com/bbear/example/lintCode/Test2.java
+++ src/main/java/com/bbear/example/lintCode/Test2.java
@@ -1,8 +1,65 @@
 package com.bbear.example.lintCode;
 
+import java.util.Stack;
+
 /**
+ * 实现一个带有取最小值min方法的栈，min方法将返回当前栈中的最小值。
+ 你实现的栈将支持push，pop 和 min 操作，所有操作要求都在O(1)时间内完成。
+ 栈的特点：先进后出
+
  * @author junxiongchen
  * @date 2018/02/23
  */
 public class Test2 {
+    Stack<Integer> s;
+    Stack<Integer> fuzhu;
+
+    public Test2() {
+        s = new Stack<>();
+        fuzhu = new Stack<>();
+    }
+    public static void main(String[] args) throws Exception {
+        Test2 test2 = new Test2();
+        test2.push(13);
+        test2.push(7);
+        test2.push(11);
+        test2.push(2);
+        test2.push(1);
+        test2.pop();
+        System.out.println(test2.min());
+        System.out.println(test2.s);
+        System.out.println(test2.fuzhu);
+    }
+    //压入堆的顶部
+    private  void push(int number) {
+        s.push(number);
+        if (fuzhu.isEmpty() == true) {
+            fuzhu.push(number);
+        } else {
+            //查看堆顶部的对象但是不移除
+            Integer peek = fuzhu.peek();
+            if (peek < number) {
+                fuzhu.push(peek);
+            } else {
+                fuzhu.push(number);
+            }
+        }
+    }
+    //移除堆顶部的对象并且返回
+    private int pop() throws Exception {
+        if (s.isEmpty() == true) {
+            throw new Exception("stack is null");
+        } else {
+            fuzhu.pop();
+            return s.pop();
+        }
+    }
+
+    private int min() throws Exception {
+        if (fuzhu.isEmpty() == true) {
+            throw new Exception("min stack is null");
+        } else {
+            return fuzhu.peek();
+        }
+    }
 }
--- src/main/java/com/bbear/example/lintCode/Test3.java
+++ src/main/java/com/bbear/example/lintCode/Test3.java
@@ -1,8 +1,65 @@
 package com.bbear.example.lintCode;
 
+import java.util.Stack;
+
 /**
+ * 使用两个栈来实现队列，支持push,pop,top
+ * 实现push(1), pop(), push(2), push(3), top(), pop()，你应该返回1，2和2
+ * 队列的特点：先进后出
+ *
  * @author junxiongchen
  * @date 2018/02/24
  */
 public class Test3 {
+    /**
+     * 入栈
+     */
+    Stack<Integer> stack;
+    /**
+     * 出栈
+     */
+    Stack<Integer> stack1;
+
+    public Test3() {
+        stack = new Stack<>();
+        stack1 = new Stack<>();
+    }
+
+    public static void main(String[] args) {
+        Test3 test3 = new Test3();
+        test3.push(1);
+        System.out.println(test3.pop());
+        test3.push(2);
+        test3.push(3);
+        System.out.println(test3.top());
+        System.out.println(test3.pop());
+    }
+
+    private void push(int number) {
+        stack.push(number);
+    }
+
+    private int pop() {
+        if (!stack1.empty()) {
+            return stack1.pop();
+        } else {
+            while (stack.size() > 0) {
+                stack1.push(stack.pop());
+            }
+            System.out.println("stack:" + stack);
+            System.out.println("stack1" + stack1);
+            return stack1.pop();
+        }
+    }
+
+    private int top() {
+        if (!stack1.empty()) {
+            return stack1.pop();
+        } else {
+            while (stack.size() > 0) {
+                stack1.push(stack.pop());
+            }
+            return stack1.peek();
+        }
+    }
 }
