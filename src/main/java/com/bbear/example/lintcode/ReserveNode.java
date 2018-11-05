cb964e0adeabfac7fa6e7b2392c9ca77c8f3c00c
BBear <chenjx0515@163.com>
2018-11-05 18:34:33 +0800

    is_realTime bug

--- src/main/java/com/bbear/example/lintcode/ReserveNode.java
+++ src/main/java/com/bbear/example/lintcode/ReserveNode.java
@@ -1,29 +1,54 @@
 package com.bbear.example.lintcode;
 
+
+import org.w3c.dom.Node;
+import java.util.Stack;
+
 /**
- * 反转单链表
+ * 反转单链表 ,暂时忽略Node的问题
  * 1->2->3->null ,反转后 null<-1<-2-<3
  *
  * @author junxiongchen
  * @date 2018/03/07
  */
 public class ReserveNode {
-    /*ActList* reserveList(ActList* head){
-        if (Null == head || Null == head->next){
-            return head;
+    /**
+     *  利用栈的先进后出
+     * @param node
+     */
+    private void reserveNode1(Node node){
+        Stack<Node> stack = new Stack<>();
+        while (node != null) {
+            stack.push(node);
+            node = node.getNextSibling();
         }
-        ActList* p,q,r;
-        p = head;
-        q = head->next;
-        head->next = null;
-        while(q){
-            r = q->next;
-            q->next = p;
-            p = q;
-            q = r;
+        while (stack != null) {
+            System.out.println("stack.pop=====>" + stack.pop());
         }
-        //最后q指向null，所以p作为新的指针头
-        head = p;
-        return head;
-    }*/
+    }
+
+    /**
+     * 头插法插入链表
+     * @param head
+     */
+    private void reserveNode2(Node head){
+        if (head == null) {
+            return;
+        }
+        /**翻转之后的链表*/
+        Node node ;
+
+        Node pre = head;
+        Node cur = head.getNextSibling();
+        Node next;
+        while (cur != null){
+            next = cur.getNextSibling();
+        //    cur.getNextSibling() = pre;
+            pre = cur;
+            cur = next;
+        }
+     //   head.getNextSibling() = null;
+        node = pre;
+    }
+
 }
